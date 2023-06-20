2bbd55d09a174ae829de72ca1cdb954a07d2162b
JesusGarSan <128600457+JesusGarSan@users.noreply.github.com>
2023-06-20 17:58:47 +0200

    Add files via upload

--- Simulaciones/Snell.py
+++ Simulaciones/Snell.py
@@ -44,8 +44,17 @@ from st_pages import Page, Section, show_pages, show_pages_from_config, add_page
 
 
 # ---------------------------------------------------------------------- CONTENIDO PROPIO DE LA PÁGINA ----------------------------------------------------------------------
-
+column = st.columns(2)
+column_0 = column[0].columns(2) 
 #------------------------------------------------------------------------- DEFINICIÓN DE FUNCIONES ------------------------------------------------------------------------
+# Coordenadas polares
+
+def polares(centro, r, theta):
+    extremo=[0,0]
+    extremo[0] =  r*np.sin(np.pi*theta/180)
+    extremo[1] =  r*np.cos(np.pi*theta/180)
+
+    return extremo
 
 class rayo:
     def __init__(self, origen, longitud ,theta, sentido):
@@ -53,8 +62,9 @@ class rayo:
 
         self.origen = np.array(origen)
         self.longitud = longitud
-        self.sentido = sentido
-        self.theta = np.abs(theta)
+        self.theta = theta
+        self.sentido = +1
+        if 90 < self.theta and self.theta < 270: self.sentido = -1        
         self.extremo = np.array(polares(self.origen, longitud, theta)) + np.array(self.origen)
 
 
@@ -66,6 +76,41 @@ class rayo:
               sentido: {self.sentido}
               """)
 
+    
+
+
+            
+    def plot_rayo(self, color, arrow):
+        x=[self.origen[0], self.extremo[0]]
+        y=[self.origen[1], self.extremo[1]]
+        plt.plot(x, y, color=color, label='Rayo incidente')
+        if arrow==True:
+            plt.arrow( (x[0]+x[1])/2, (y[0]+y[1])/2, 0.02*np.sin(np.pi*self.theta/180), +0.02*np.cos(np.pi*self.theta/180), width=.008, color=color, length_includes_head=True)
+
+
+    def reflejar(self, longitud=10):
+        if self.sentido == -1:rayo_reflejado = rayo(origen= self.extremo, longitud=longitud, theta= 180-self.theta, sentido=-self.sentido)
+        if self.sentido == +1:
+            rayo_reflejado = rayo(origen= self.extremo, longitud=longitud, theta=180-self.theta, sentido=-self.sentido)
+            
+            rayo_reflejado.extremo[0] += np.abs(rayo_reflejado.origen[0]-rayo_reflejado.extremo[0]) *2
+        #rayo_reflejado = rayo(origen= self.extremo, longitud=longitud, theta=180-self.theta, sentido=self.sentido)
+        rayo_reflejado.medio = self.medio
+        
+        return rayo_reflejado
+
+    def refractar(self, n_1, n_2):
+        theta = snell(n_1, n_2, self.theta)
+        if theta!=None:
+            if self.sentido==-1:rayo_refractado = rayo(origen= self.extremo, longitud=10 ,theta= 180-theta, sentido=self.sentido)
+            if self.sentido==+1:rayo_refractado = rayo(origen= self.extremo, longitud=10 ,theta= theta, sentido=self.sentido)
+
+            if self.sentido==-1: rayo_refractado.medio = self.medio+1
+            if self.sentido==+1: rayo_refractado.medio = self.medio-1
+
+            return rayo_refractado
+        else: return None
+
     def recortar(self, fronteras, plot_colision):
 
         recortado = False
@@ -73,7 +118,6 @@ class rayo:
         # Localizamos las fronteras que se encuentran entre el origen y el extremo actual
         if self.sentido== +1:
             fronteras_intermedias = np.where((self.origen[1]<fronteras) & (self.extremo[1]>fronteras))[0]
-            print(fronteras[fronteras_intermedias])
         if self.sentido== -1:
             fronteras_intermedias = np.where((self.origen[1]>fronteras) & (self.extremo[1]<fronteras))[0]
         # Reasiganmos el nuevo valor de extremo (si se han encontrado fronteras intermedias)
@@ -81,11 +125,11 @@ class rayo:
             if self.sentido== +1: indice_nuevo_extremo = fronteras_intermedias[len(fronteras_intermedias)-1]
             if self.sentido== -1: indice_nuevo_extremo = fronteras_intermedias[0]
             nuevo_extremo = fronteras[indice_nuevo_extremo]
-            print(nuevo_extremo)
 
-            if self.sentido ==-1: self.extremo[0] = nuevo_extremo / self.extremo[1] * self.extremo[0]
+            if self.sentido ==-1:
+                self.extremo[0] = np.tan((180-self.theta)*np.pi/180)*np.abs(self.origen[1]-nuevo_extremo)
+                self.extremo[0]+=self.origen[0]
             if self.sentido ==+1:
-                #self.extremo[0] = np.abs(nuevo_extremo - self.origen[1]) / np.abs(self.extremo[1]-self.origen[1]) * self.extremo[0]
                 self.extremo[0] = np.tan(self.theta*np.pi/180)*np.abs(self.origen[1]-nuevo_extremo)
                 self.extremo[0]+=self.origen[0]
             self.extremo[1] = nuevo_extremo
@@ -98,38 +142,6 @@ class rayo:
         return recortado
 
 
-            
-    def plot_rayo(self, color, arrow):
-        x=[self.origen[0], self.extremo[0]]
-        y=[self.origen[1], self.extremo[1]]
-        plt.plot(x, y, color=color, label='Rayo incidente')
-        if arrow==True:
-            if self.sentido == -1: plt.arrow( (x[0]+x[1])/2, (y[0]+y[1])/2, 0.02*np.sin(np.pi*self.theta/180), -0.02*np.cos(np.pi*self.theta/180), width=.008, color=color, length_includes_head=True)
-            if self.sentido == +1: plt.arrow( (x[0]+x[1])/2, (y[0]+y[1])/2, 0.02*np.sin(np.pi*self.theta/180), +0.02*np.cos(np.pi*self.theta/180), width=.008, color=color, length_includes_head=True)
-
-
-    def reflejar(self):
-        rayo_reflejado = rayo(origen= self.extremo, longitud=1, theta= self.theta, sentido=-self.sentido)
-
-        return rayo_reflejado
-
-    def refractar(self, n_1, n_2):
-        theta = snell(n_1, n_2, self.theta)
-        if theta!=None:
-            rayo_refractado = rayo(origen= self.extremo, longitud=10 ,theta= theta, sentido=self.sentido)
-            rayo_refractado.extremo[1] = np.abs(rayo_refractado.extremo[1]) * rayo_refractado.sentido
-            return rayo_refractado
-        else: return None
-
-# Coordenadas polares
-
-def polares(centro, r, theta):
-    extremo=[0,0]
-    extremo[0] =  r*np.sin(np.pi*theta/180)
-    extremo[1] =  r*np.cos(np.pi*theta/180)
-
-    return extremo
-
 # Ley de Snell
 # n_1 * sin(O_1) = n_2 * sin(O_2)
 def snell(n_1, n_2, O_1):
@@ -137,10 +149,44 @@ def snell(n_1, n_2, O_1):
     return np.arcsin( n_1/n_2 * np.sin(O_1/180*np.pi) ) * 180/np.pi
 
 
+def incidencia(rayo_incidente, fronteras, n, call, plot_reflexiones, plot_refracciones):
+    if call >=5: return
+    if rayo_incidente.extremo[0]>1: return
+    print(call)
+
+    #Reflexión
+    if plot_reflexiones or call==0:
+        rayo_reflejado = rayo_incidente.reflejar(longitud=1.5)
+        recurrencia_reflexion = rayo_reflejado.recortar(fronteras, False)
+
+        rayo_reflejado.plot_rayo('green', arrow=True)
+        if recurrencia_reflexion:
+            incidencia(rayo_reflejado, fronteras, n, call+1, plot_reflexiones, plot_refracciones)
+    if plot_refracciones==False and call>1: return
+
+    # Refracción
+    i_medio = rayo_incidente.medio
+    if rayo_incidente.sentido==-1:
+        if len(n)<=i_medio+1: return
+        rayo_refractado = rayo_incidente.refractar(n[i_medio], n[i_medio+1] )
+    if rayo_incidente.sentido==+1:
+        if len(n)<=i_medio: return
+        rayo_refractado = rayo_incidente.refractar(n[i_medio], n[i_medio-1] )
+    if rayo_refractado==None: return # Reflexión Interna Total
+
+    recurrencia_refraccion = rayo_refractado.recortar(fronteras, False)
+
+    rayo_refractado.plot_rayo('red', arrow=True)
+
+    if recurrencia_refraccion:
+        incidencia(rayo_refractado, fronteras, n, 1, plot_reflexiones, plot_refracciones)
+
+    return
+
+
 #Dibujado y ploteo del simulador
-def simulador_snell(n_1, n_x, O_1, n_medios, plot_reflexiones):
 
-
+def simulador_snell(n_1, n_x, O_1, n_medios, plot_reflexiones, plot_refracciones):
 
     fig, ax = plt.subplots(figsize=(12,10))
     ax.axis('off')
@@ -170,123 +216,18 @@ def simulador_snell(n_1, n_x, O_1, n_medios, plot_reflexiones):
     x = -np.sin(np.pi*O_1/180)
     y = +np.cos(np.pi*O_1/180)
 
-    rayo_incidente = rayo(origen = [0,0], longitud = longitud_rayos, theta = -O_1, sentido=-1) #Creamos el rayo como objeto
+    rayo_incidente = rayo(origen = [0,0], longitud = longitud_rayos, theta = 180-O_1, sentido=-1) #Creamos el rayo como objeto
+    rayo_incidente.medio = 0    
     aux = rayo_incidente.extremo
     rayo_incidente.extremo = rayo_incidente.origen
-    rayo_incidente.origen = aux
+    rayo_incidente.origen = -aux
+
     rayo_incidente.recortar(fronteras, True)
     rayo_incidente.plot_rayo('blue', arrow= True)
 
 
+    incidencia(rayo_incidente, fronteras, n, 0, plot_reflexiones, plot_refracciones)
 
-
-    aux_rayo_incidente = copy.deepcopy(rayo_incidente)
-    for i in range (n_medios):
-        # Reflexiones
-        if plot_reflexiones or i == 0:
-            rayo_reflejado = aux_rayo_incidente.reflejar(); rayo_reflejado.stats()
-            if i == 0: rayo_reflejado.recortar(fronteras, plot_colision=False)
-            if i != 0: rayo_reflejado.recortar(fronteras, plot_colision=True); rayo_reflejado.stats()
-            rayo_reflejado.plot_rayo('green', arrow=True)
-        # Refracciones
-        rayo_refractado = aux_rayo_incidente.refractar(n[i], n[i+1] )
-        if rayo_refractado!=None:
-            rayo_refractado.recortar(fronteras, plot_colision=True)
-            rayo_refractado.plot_rayo('red', arrow= False)
-            if i==n_medios//2: rayo_refractado.plot_rayo('red', arrow= True)
-        
-            aux_rayo_incidente = copy.deepcopy(rayo_refractado)
-
-        if plot_reflexiones and i+1 == n_medios:
-            rayo_reflejado = aux_rayo_incidente.reflejar(); rayo_reflejado.stats()
-            rayo_reflejado.recortar(fronteras, plot_colision=True); rayo_reflejado.stats()
-            rayo_reflejado.plot_rayo('green', arrow=True)
-
-
-    return fig
-
-    
-    inc_x = [extremos[0], 1]
-    inc_y = [extremos[1], -1]
-
-    #inc_x = [-longitud_rayos * np.sin(np.pi*O_1/180),0]
-    #inc_y = [longitud_rayos * np.cos(np.pi*O_1/180),0]
-
-    color_incidente='blue'
-
-    ax.fill_between([-1,1], [1,1], color='white') # medio 1
-    ax.vlines(0, -1, 1, color='grey', linestyles='dashed') # vertical
-    ax.plot(inc_x, inc_y, color=color_incidente, label='Rayo incidente')
-    ax.arrow( (inc_x[0]+inc_x[1])/2, (inc_y[0]+inc_y[1])/2, 0.02*np.sin(np.pi*O_1/180), -0.02*np.cos(np.pi*O_1/180), width=.008, color=color_incidente, length_includes_head=True)
-    return fig
-    # Rayo Reflejado
-
-    longitud_rayos = .8
-    reflejado_x = [longitud_rayos * np.sin(np.pi*O_1/180),0]
-    reflejado_y = [longitud_rayos * np.cos(np.pi*O_1/180),0]
-
-    color_reflexion = 'green'
-
-    ax.vlines(0, -1, 1, color='grey', linestyles='dashed') # vertical
-    ax.plot(reflejado_x, reflejado_y, color=color_reflexion, label='Rayo reflejado')
-    ax.arrow( (reflejado_x[0]+reflejado_x[1])/2, (reflejado_y[0]+reflejado_y[1])/2, 0.02*np.sin(np.pi*O_1/180), 0.02*np.cos(np.pi*O_1/180),
-            width=.008, color=color_reflexion, length_includes_head=True)
-
-
-
-
-
-    # Rayos refractados
-    O_2=O_1
-    refrac_x=[0,0]
-    refrac_y=[0,0]
-    color_refrac = 'red'
-    dibujar_flecha=True
-    for i in range(n_medios):
-        O_1 = O_2
-        O_2 = snell(n_1, n_1+(i+1)*incremento_n, O_1)
-
-
-
-        if O_2!=None:
-            longitud_segmentos = 1/n_medios/np.cos(np.pi*O_2/180)
-
-            refrac_x = [0 + refrac_x[1], refrac_x[1] + longitud_segmentos * np.sin(np.pi*O_2/180)]
-            refrac_y = [0 + refrac_y[1], refrac_y[1] + -longitud_segmentos * np.cos(np.pi*O_2/180)]
-
-            # Dibujamos la flecha de dirección correctamente si sólo hay 1 medio refractante
-            if n_medios == 1:
-                if refrac_x[1] > 0.5: divisor = refrac_x[1]*2
-                ax.arrow( (refrac_x[0]+refrac_x[1])/divisor, (refrac_y[0]+refrac_y[1])/divisor, 0.02*np.sin(np.pi*O_2/180), -0.02*np.cos(np.pi*O_2/180), width=.008, color=color_refrac, length_includes_head=True)
-                dibujar_flecha=False
-            # Dibujamos la flecha de dirección
-            if (refrac_x[0]>0.5 or refrac_y[0]<-0.5) and dibujar_flecha:
-                ax.arrow( refrac_x[0], refrac_y[0], 0.02*np.sin(np.pi*O_2/180), -0.02*np.cos(np.pi*O_2/180), width=.008, color=color_refrac, length_includes_head=True)
-                dibujar_flecha=False
-
-            # Sólo añadimos a la leyenda el primer rayo refractado
-            if i==0: plt.plot(refrac_x, refrac_y, color=color_refrac, label='Rayo refractado')
-            else: plt.plot(refrac_x, refrac_y, color=color_refrac)
-        else:   
-            if dibujar_flecha==True and i>0:
-                ax.arrow( refrac_x[0], refrac_y[0], 0.02*np.sin(np.pi*O_1/180), -0.02*np.cos(np.pi*O_1/180), width=.008, color=color_refrac, length_includes_head=True)
-            #break
-
-        # Rayos reflejados de los refractados
-        O_1=O_2
-        if i>0:
-            longitud_rayos = (longitud_segmentos - refrac_y[1]) / np.sin(np.pi*O_1/180)
-            reflejado_x = [longitud_rayos * np.sin(np.pi*O_1/180) + refrac_x[1], refrac_x[0]]
-            reflejado_y = [longitud_rayos * np.cos(np.pi*O_1/180) + refrac_y[1], refrac_y[0]]
-            
-            ax.vlines(refrac_x[0], -longitud_segmentos/2 + refrac_y[0], longitud_segmentos/2 + refrac_y[0], color='grey', linestyles='dashed') # vertical
-
-            ax.plot(reflejado_x, reflejado_y, color=color_reflexion)
-            #ax.arrow( (reflejado_x[0]+reflejado_x[1])/2, (reflejado_y[0]+reflejado_y[1])/2, 0.02*np.sin(np.pi*O_1/180), 0.02*np.cos(np.pi*O_1/180),
-            #        width=.008, color=color_reflexion, length_includes_head=True)
-
-    ax.legend(fontsize=16, loc=1)
-    
     return fig
 
 
@@ -294,17 +235,21 @@ def simulador_snell(n_1, n_x, O_1, n_medios, plot_reflexiones):
 # ----------------------------------------------------------------------------------- SIMULACIÓN --------------------------------------------------------------------------
 
 # Parámetros iniciales
-column = st.columns(2)
-column_0 = column[0].columns(2) 
+
 n_1 = column_0[0].number_input('Índice de refracción del medio SUPERIOR', 1.,5., value = 1.,format='%.3f')
 n_x = column_0[0].number_input('Índice de refracción del medio INFERIOR', 1.,5., value = 1.2, format='%.3f')
 plot_reflexiones = column_0[0].checkbox('Dibujar Reflexiones Internas')
+plot_refracciones = column_0[0].checkbox('Dibujar Refracciones Internas')
+if plot_reflexiones==False: plot_refracciones=False
 
 
 O_1 = column_0[1].slider('Ángulo de incidencia (º)', 0, 90, value=60)
 n_medios = column_0[1].slider('Número de medios', 2,40)
 n_medios-=1
-column[1].pyplot(simulador_snell(n_1, n_x, O_1, n_medios, plot_reflexiones=plot_reflexiones))
+
+
+
+column[1].pyplot(simulador_snell(n_1, n_x, O_1, n_medios, plot_reflexiones, plot_refracciones))
 
 
 
@@ -321,7 +266,5 @@ column[1].pyplot(simulador_snell(n_1, n_x, O_1, n_medios, plot_reflexiones=plot_
 #cuestionario.mostrar_cuestionario()
 
 # ----------------------------------------------------------------------------- FOOTER ----------------------------------------------------------------------------------
-
-
 
 
