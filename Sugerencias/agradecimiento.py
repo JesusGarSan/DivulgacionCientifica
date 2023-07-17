ad0fdc8abbb60ae43761413bf893a9de11f0d3da
JesusGarSan <128600457+JesusGarSan@users.noreply.github.com>
2023-07-17 09:10:44 +0200

    Add files via upload

---
+++ Sugerencias/agradecimiento.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# ----------------------------------------------------------------- CONFIGURACIÓN INICIAL DE LA PÁGINA -----------------------------------------------------------------
+import setup_page
+setup_page.setup_page(width='centered',)
+
+# ------------------------------------------------------------------- CONTENIDO PROPIO DE LA PÁGINA --------------------------------------------------------------------
+
+st.header('¡Gracias por tu mensaje!')
+st.markdown('Leeremos tu mensaje en cuanto podamos. Mientras tanto, siéntete libre de seguir navegando nuestra página')
+
+
